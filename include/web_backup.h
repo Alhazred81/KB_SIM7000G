36e6290dce64eb773a8271ac51c4e82a04aeac3d
Balazs <krupanszkib@gmail.com>
2026-08-16 16:58:06 +0200

    EEPROM backup/restore separated into web_backup module

---
+++ include/web_backup.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void handleEepromBackup();
+void handleEepromRestore();
--- include/web_ui.h
+++ include/web_ui.h
@@ -13,6 +13,7 @@
 #include "sensors.h"
 #include "web_common.h"
 #include "web_theme.h"
+#include "web_backup.h"
 
 extern WebServer server;
 extern DNSServer dnsServer;
@@ -1428,97 +1429,6 @@ void handleChangePin() {
   else diagAdd("SIM PIN csere HIBA: "+err);
   server.sendHeader("Location","/cfg");
   server.send(302);
-}
-
-void handleEepromBackup() {
-  uint8_t buf[EEPROM_SIZE];
-  for(int i = 0; i < EEPROM_SIZE; i++) buf[i] = EEPROM.read(i);
-
-  uint32_t checksum = 0;
-  for(int i = 0; i < EEPROM_SIZE; i++) checksum = (checksum * 31) + buf[i];
-
-  String b64 = base64Encode(buf, EEPROM_SIZE);
-
-  String html = htmlHead("EEPROM export", "5");
-  html += "<h1>Beallitasok exportja</h1>";
-  html += "<div class='card full'>";
-  html += "<p class='hint'>Masold ki es mentsd el ezt a szoveget egy biztonsagos helyre.</p>";
-  html += "<textarea readonly style='min-height:140px;font-family:monospace;font-size:11px' "
-          "onclick='this.select()'>" + String(EEPROM_SIZE) + ":" + String(checksum) + ":" + b64 + "</textarea>";
-  html += "<a href='/diag'><button class='sec' style='margin-top:10px'>Vissza</button></a>";
-  html += "</div>";
-  html += htmlFoot();
-  server.send(200, "text/html", html);
-}
-
-void handleEepromRestore() {
-  String html = htmlHead("EEPROM visszatoltes", "5");
-  html += "<h1>Beallitasok visszatoltese</h1>";
-
-  if(!server.hasArg("data") || server.arg("data").length() == 0) {
-    html += "<div class='msg err'>Nincs beillesztett adat.</div>";
-    html += "<a href='/diag'><button class='sec'>Vissza</button></a>";
-    html += htmlFoot();
-    server.send(200, "text/html", html);
-    return;
-  }
-
-  String data = server.arg("data");
-  data.trim();
-
-  int c1 = data.indexOf(':');
-  int c2 = (c1 >= 0) ? data.indexOf(':', c1 + 1) : -1;
-  if(c1 < 0 || c2 < 0) {
-    html += "<div class='msg err'>Ervenytelen formatum - hianyzik a fejlec.</div>";
-    html += "<a href='/diag'><button class='sec'>Vissza</button></a>";
-    html += htmlFoot();
-    server.send(200, "text/html", html);
-    return;
-  }
-
-  int declaredSize = data.substring(0, c1).toInt();
-  uint32_t declaredChecksum = (uint32_t)data.substring(c1 + 1, c2).toInt();
-  String b64 = data.substring(c2 + 1);
-
-  if(declaredSize != EEPROM_SIZE) {
-    html += "<div class='msg err'>Meret-eltero export (" + String(declaredSize) + " byte).</div>";
-    html += "<a href='/diag'><button class='sec'>Vissza</button></a>";
-    html += htmlFoot();
-    server.send(200, "text/html", html);
-    return;
-  }
-
-  uint8_t buf[EEPROM_SIZE];
-  size_t decoded = base64Decode(b64, buf, EEPROM_SIZE);
-
-  if(decoded != (size_t)EEPROM_SIZE) {
-    html += "<div class='msg err'>Hianyos/serult adat.</div>";
-    html += "<a href='/diag'><button class='sec'>Vissza</button></a>";
-    html += htmlFoot();
-    server.send(200, "text/html", html);
-    return;
-  }
-
-  uint32_t checksum = 0;
-  for(int i = 0; i < EEPROM_SIZE; i++) checksum = (checksum * 31) + buf[i];
-
-  if(checksum != declaredChecksum) {
-    html += "<div class='msg err'>Ellenorzo osszeg hiba - az adat serult.</div>";
-    html += "<a href='/diag'><button class='sec'>Vissza</button></a>";
-    html += htmlFoot();
-    server.send(200, "text/html", html);
-    return;
-  }
-
-  for(int i = 0; i < EEPROM_SIZE; i++) EEPROM.write(i, buf[i]);
-  EEPROM.commit();
-
-  diagAdd("EEPROM visszatoltve (manualis).");
-  html += "<div class='msg ok'>Beallitasok sikeresen visszatoltve!</div>";
-  html += "<form action='/reinit' method='POST'><button class='warn'>Modem ujraindit</button></form>";
-  html += "<a href='/'><button class='sec' style='margin-top:8px'>Fooldal</button></a>";
-  html += htmlFoot();
-  server.send(200, "text/html", html);
 }
 
 void handleDiag() {
